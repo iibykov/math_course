40b15112e62d4271d2dd62d7c15e51e628ce28ca
i00535886 <ilya.bykov@huawei.com>
2020-12-08 21:42:13 +0300

    Create 1_4.py
    A linear combination of the vectors

---
+++ 1_4.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+p = np.array([2, 4, 5])
+v = np.array([8, 10, 2])
+s = np.array([0, 12, 7])
+
+print(500*p + 100*v + 0*s)
+
+# 1.4.1
+x = np.array([4, 5])
+y = np.array([2, 1])
+u = np.array([1, 0])
+print(2*x + -3*y + 5*u)
+
+# 1.4.2
+a1 = np.array([3, 4, 5, 9])
+b1 = np.array([1, 5, 3, 6])
+
+print(400*b1 - 200*a1)
